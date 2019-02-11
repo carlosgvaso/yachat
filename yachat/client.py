7d16477d196aca224a2ae80f58e6b26a5608a5a5
Jose C.M. Garcia-Vaso <carlosgvaso@gmail.com>
2019-02-10 18:51:57 -0600

    Added signal handler and listener thread.

--- yachat/client.py
+++ yachat/client.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 
-import argparse     # Parsing command-line arguments
-import logging      # Logging
-#from sys import exit
-
-import socket
-
-# from _thread import *
-import threading
+import argparse         # Parsing command-line arguments
+import logging          # Logging
+import signal           # Signal managing
+from time import sleep  # Sleeping
+import socket           # TCP and UDP sockets
+import threading        # Multi-threading
+import queue            # Queue to share data between threads
 
 
 ##
@@ -20,6 +19,7 @@ default_log_level = 'WARN'
 err_ok = 0
 err_arg = 1
 err_join = 2
+err_user = 3
 
 # Protocol
 proto_tcp_helo = 'HELO {0} {1} {2}\n'   # HELO <screen_name> <IP> <Port>\n
@@ -30,6 +30,9 @@ proto_udp_join = 'JOIN {0} {1} {2}\n'   # JOIN <screen_name> <IP> <Port>\n
 proto_udp_mesg = 'MESG {0}: {1}\n'      # MESG <screen_name>: <message>\n
 proto_udp_exit = 'EXIT {0}\n'           # EXIT <screen_name>\n
 
+# Signals
+signals_to_names = dict((getattr(signal, n), n) for n in dir(signal) if n.startswith('SIG') and '_' not in n)
+
 
 ##
 # Classes
@@ -38,12 +41,13 @@ class Chatter:
     """ Chat client main class.
     """
 
-    def __init__(self, screen_name, server_hostname, server_welcome_port):
+    def __init__(self, screen_name, server_hostname, server_welcome_port, sleep_time=1):
         """ Initialize instance variables.
 
             :param  screen_name         Username to register in the membership server.
             :param  server_hostname     Chat membership server's hostname.
             :param  server_welcome_port Welcome port of the chat membership server.
+            :param  sleep_time          Time to pause in between loops in sec.
         """
         # Membership server info in the format:
         # { hostname, welcome_port }
@@ -55,22 +59,39 @@ class Chatter:
         # The first entry is this instance of the client.
         self.clients = [{'screen_name': screen_name, 'ip': None, 'udp_port': None}]
 
-        # Sockets
-        self.s_server_tcp = None
-        self.s_server_udp = None
+        # Other data structures
+        self.msg_leftovers_tcp = bytes()  # If we receive the beginning of the next msg, save it here
+        self.q_listener = queue.Queue()  # Listener thread queue
+        self.q_reader = queue.Queue()  # Reader thread queue
 
-        self.msg_leftovers_tcp = bytes()    # If we receive the beginning of the next msg, save it here
+        # Flags and signal handlers
+        self.flag_run = True
+        self.original_sigint = None
+        self.original_sigterm = None
+
+        # Sockets
+        self.s_server_tcp = None    # TCP port connected to the chat membership server
+        self.s_server_udp = None    # UDP server port to listen for and send messages
+
+        self.sleep_time = sleep_time        # Time to pause in between loops in sec
 
         logging.debug('Initial Chatter configuration:\n\tchat_server = {0}\n\tclients = {1}'
                       .format(self.chat_server, self.clients))
 
     def create_udp_port(self):
         """ Create UDP port to talk to other clients.
-
-            TODO: implement method.
         """
-        self.clients[0]['ip'] = 'localhost'
-        self.clients[0]['udp_port'] = 8800
+        logging.info('Creating UDP socket...')
+        # Find client's IP address
+        self.clients[0]['ip'] = socket.gethostbyname(socket.gethostname())
+
+        # Create a UDP socket
+        self.s_server_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s_server_udp.bind((self.clients[0]['ip'], 0))
+
+        # Save IP and port
+        self.clients[0]['ip'], self.clients[0]['udp_port'] = self.s_server_udp.getsockname()
+        logging.debug('UDP socket: ip = {0}, port = {1}'.format(self.clients[0]['ip'], self.clients[0]['udp_port']))
 
     def exit_server(self):
         """ Exit chat server.
@@ -100,7 +121,7 @@ class Chatter:
         """
         logging.info('Joining the chat membership server...')
 
-        # create an INET, STREAMing socket, and connect to the chat server
+        # create TCP socket, and connect to the chat server
         self.s_server_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s_server_tcp.connect((self.chat_server['hostname'], self.chat_server['welcome_port']))
 
@@ -127,6 +148,17 @@ class Chatter:
 
         return True
 
+    def stop(self, sig, func=None):
+        """ Do required tasks to stop the client.
+        """
+        # Restore the original signal handlers to allow to stop forcefully
+        signal.signal(signal.SIGINT, self.original_sigint)
+        signal.signal(signal.SIGTERM, self.original_sigterm)
+
+        logging.warning('Signal %s received: Exiting gracefully (you can press [Ctrl]+[D] again to stop forcefully)...',
+                        signals_to_names[sig])
+        self.flag_run = False
+
     def process_client_list(self, response_str):
         """ Process client list from ACPT message to self.clients list.
 
@@ -151,22 +183,106 @@ class Chatter:
                 # Confirm IP and UDP port
                 if client_info[1] != self.clients[0]['ip'] or client_info[2] != self.clients[0]['udp_port']:
                     logging.error('Server has the wrong IP and/or port for this client')
-                    self.exit_server()
-                    exit(err_join)
+                    self.stop(signal.SIGTERM)
+                    return
                 else:
                     logging.info('Server has the correct IP and port for this client')
             else:
                 # Add new client to clients structure
                 self.clients.append({'screen_name': client_info[0], 'ip': client_info[1], 'udp_port': client_info[2]})
                 logging.debug('Added new client: {0}'.format(self.clients[-1]))
+                print('{0} is in the chatroom'.format(client_info[0]))
 
         logging.debug('self.clients = {0}'.format(self.clients))
         logging.info('Client list filled')
 
+    def process_exit_message(self, msg_list):
+        """ Process UDP EXIT message.
+
+            Remove the client that exited from the clients data structure.
+
+            :param  msg_list    List of space separated words of 'EXIT' message.
+        """
+        # Check we got the right message type
+        if msg_list[0] != 'EXIT':
+            logging.error('Wrong message type received for processing: {0}'.format(msg_list))
+            return
+
+        client_exited = msg_list[1]
+
+        # Remove client
+        for client in self.clients:
+            if client_exited == client['screen_name']:
+                self.clients.remove(client)
+                logging.info('Client {0} left the server'.format(client['screen_name']))
+                print('{0} left the chatroom'.format(client['screen_name']))
+                logging.debug('self.clients = {0}'.format(self.clients))
+
+    def process_join_message(self, msg_list):
+        """ Process UDP JOIN message.
+
+            Get the information of the new client, and add it to the clients data structure if the client is not this
+            instance. If it is this instance, check that the information is corect.
+
+            :param  msg_list    List of space separated words of 'JOIN' message.
+        """
+        # Check we got the right message type
+        if msg_list[0] != 'JOIN':
+            logging.error('Wrong message type received for processing: {0}'.format(msg_list))
+            return
+
+        msg_list[3] = int(msg_list[3])  # Convert the port number to int
+
+        # Check if this client is the one that joined
+        if msg_list[1] == self.clients[0]['screen_name']:
+            if msg_list[2] != self.clients[0]['ip'] or msg_list[3] != self.clients[0]['udp_port']:
+                logging.error('Server has the wrong IP and/or port for this client')
+                self.stop(signal.SIGTERM)
+                return
+            else:
+                logging.info('Server has the correct IP and port for this client')
+        else:
+            # Add new client to clients data structure
+            self.clients.append({'screen_name': msg_list[1], 'ip': msg_list[2], 'udp_port': msg_list[3]})
+            logging.debug('Added new client: {0}'.format(self.clients[-1]))
+            logging.info('Client {0} joined the server'.format(msg_list[1]))
+            print('{0} joined the chatroom'.format(msg_list[1]))
+
+    def process_mesg_message(self, msg_list):
+        """ Process UDP MESG message.
+
+            :param  msg_list    List of space separated words of 'MESG' message.
+        """
+        # Check we got the right message type
+        if msg_list[0] != 'MESG':
+            logging.error('Wrong message type received for processing: {0}'.format(msg_list))
+            return
+
+        logging.info('Message received: {0} {1}'.format(msg_list[1], msg_list[2]))
+        print('{0} {1}'.format(msg_list[1], msg_list[2]))
+
+    def process_udp_messages(self, msg):
+        """ Process messages received over UDP.
+
+            :param  msg Message received as a bytes object.
+        """
+        # Convert message to a string, and split on white spaces
+        msg_list = str(msg.decode(encoding='utf-8')).rstrip('\n').split(' ')
+
+        # Select the function to use for the message type
+        if msg_list[0] == 'JOIN':
+            self.process_join_message(msg_list)
+        elif msg_list[0] == 'EXIT':
+            self.process_exit_message(msg_list)
+        elif msg_list[0] == 'MESG':
+            self.process_mesg_message(msg_list)
+        else:
+            logging.error('Unknown UDP message received: {}'.format(msg))
+
     def receive_tcp_msg(self, conn):
         """ Receive message over TCP socket.
 
-            :param  conn    Socket connection.
+            :param  conn    TCP socket connection.
             :return Message received as a bytes object.
 
             TODO: Make safe for faulty socket connection.
@@ -204,13 +320,20 @@ class Chatter:
             logging.debug('chunk: {0}'.format(chunk))
 
         logging.debug('msg: {0}'.format(msg))
-        logging.debug('msg_leftovers_tcp: {0}'.format(self.msg_leftovers_tcp))
+        logging.debug('msg_leftovers_udp: {0}'.format(self.msg_leftovers_tcp))
         return msg
 
     def request_new_screen_name(self):
         """ Request new screen name from the user.
         """
-        new_name = input('The screen name {0} is not available. Please, enter a new screen name: '.format(self.clients[0]['screen_name']))
+        try:
+            new_name = input('The screen name {0} is not available. Please, enter a new screen name: '
+                             .format(self.clients[0]['screen_name']))
+        except EOFError:
+            print()
+            self.stop(signal.SIGTERM)
+            return
+
         logging.debug('new_name = {0}'.format(new_name))
 
         self.clients[0]['screen_name'] = str(new_name)
@@ -223,10 +346,41 @@ class Chatter:
         self.create_udp_port()
 
         joined_server = False
-        while not joined_server:
+        while not joined_server and self.flag_run:
             joined_server = self.join_server()
 
+        # Spawn listener and reader threads
+        t_listener = Listener(0, 'listener-0', self.s_server_udp, self.q_listener)
+        t_reader = Reader(1, 'reader-1')
+
+        t_listener.start()
+        t_reader.start()
+
+        # Loop until we are told to stop running
+        while self.flag_run:
+            # Read listener queue, and process message
+            if not self.q_listener.empty():
+                msg_listener = self.q_listener.get(block=False)
+                logging.debug('Listener received: {0}'.format(msg_listener))
+
+                self.process_udp_messages(msg_listener)
+
+            # Do nothing
+            sleep(self.sleep_time)
+
+        # Send stop message to server
         self.exit_server()
+
+        # Stop all threads, and wait for them to terminate
+        t_listener.flag_run = False
+        t_reader.flag_run = False
+
+        t_listener.join()
+        t_reader.join()
+
+        # Read listener queue
+        while not self.q_listener.empty():
+            logging.debug('Listener received: {0}'.format(self.q_listener.get(block=False)))
 
     def send_tcp_msg(self, conn, msg):
         """ Send message over TCP socket.
@@ -252,37 +406,122 @@ class Chatter:
 
         return msg_sent
 
+    def set_exit_handler(self, func):
+        """ Signal handler set function.
+        """
+        # Save the original handlers
+        self.original_sigint = signal.getsignal(signal.SIGINT)
+        self.original_sigterm = signal.getsignal(signal.SIGTERM)
+
+        # Start our own signal handlers
+        signal.signal(signal.SIGTERM, func)
+        signal.signal(signal.SIGINT, func)
+
 
 class Listener (threading.Thread):
     """ Socket listener thread class.
     """
 
-    def __init__(self, thread_id, name, socket_obj):
+    def __init__(self, thread_id, name, udp_socket, msg_queue, sleep_time=1):
         """ Initialize instance variables.
 
             :param  thread_id   Thread's numeric ID.
             :param  name        Thread's name of the format "listener-<thread_id>".
-            :param  socket_obj  Socket object to listen to.
+            :param  udp_socket  UDP socket object to listen to.
+            :param  msg_queue   Message queue to share the received messages.
+            :param  sleep_time  Time to pause in between loops in sec.
         """
         threading.Thread.__init__(self)
         self.thread_id = thread_id  # Use socket port as the thread ID?
         self.name = name            # Of the format: listener-<thread_id>
-        self.socket = socket_obj
+
+        self.udp_socket = udp_socket    # UDP socket to listen to
+        self.msg_queue = msg_queue      # Message queue to share the received messages.
+
+        self.flag_run = True
+
+        self.msg_leftovers_udp = bytes()    # If we receive the beginning of the next msg, save it here
+        self.sleep_time = sleep_time        # Time to pause in between loops in sec
+
+    def receive_udp_msg(self, conn):
+        """ Receive message over UDP socket.
+
+            :param  conn    UDP socket connection.
+            :return Message received as a bytes object.
+
+            TODO: Make safe for faulty socket connection.
+        """
+        receiving = True
+
+        # Check if last time we received data, we got the beginning of the next msg
+        if self.msg_leftovers_udp != b'':
+            msg = bytes(self.msg_leftovers_udp)
+            self.msg_leftovers_udp = None       # Clear the buffer for the next time
+
+            # Check if we already got a whole second message last time
+            if b'\n' in msg:
+                receiving = False
+
+                b_tmp = msg.split(b'\n')
+                msg = b_tmp[0] + b'\n'
+                self.msg_leftovers_udp = b'\n'.join(b_tmp[1:])
+        else:
+            msg = bytes()
+
+        while receiving:
+            chunk = conn.recv(2048)
+
+            if b'\n' in chunk:
+                receiving = False
+
+                b_tmp = chunk.split(b'\n')
+                chunk = b_tmp[0] + b'\n'
+                self.msg_leftovers_udp = b'\n'.join(b_tmp[1:])
+            elif chunk == b'':
+                raise RuntimeError("Socket connection broken")
+
+            msg += chunk
+            logging.debug('chunk: {0}'.format(chunk))
+
+        logging.debug('msg: {0}'.format(msg))
+        logging.debug('msg_leftovers_udp: {0}'.format(self.msg_leftovers_udp))
+        return msg
+
+    def run(self):
+        """ Run.
+        """
+        while self.flag_run:
+            # Listen to UDP socket and put received messages in queue
+            received = self.receive_udp_msg(self.udp_socket)
+            self.msg_queue.put(received)
+
+            # Do nothing
+            sleep(self.sleep_time)
 
 
 class Reader (threading.Thread):
     """ Console reader thread class.
     """
 
-    def __init__(self, thread_id, name):
+    def __init__(self, thread_id, name, sleep_time=1):
         """ Initialize instance variables.
 
             :param  thread_id   Thread's numeric ID.
             :param  name        Thread's name of the format "reader-<thread_id>".
+            :param  sleep_time  Time to pause in between loops in sec.
         """
         threading.Thread.__init__(self)
         self.thread_id = thread_id
         self.name = name            # of the format: reader-<thread_id>
+        self.flag_run = True
+        self.sleep_time = sleep_time  # Time to pause in between loops in sec
+
+    def run(self):
+        """ Run.
+        """
+        while self.flag_run:
+            # Do stuff
+            sleep(self.sleep_time)
 
 
 class Sender (threading.Thread):
@@ -348,8 +587,9 @@ if __name__ == '__main__':
 
     logging.debug('Arguments: {0}'.format(args))
 
-    # Create and run Chatter object
+    # Create, set up signal handler and run Chatter object
     chatter = Chatter(args.screen_name, args.server_hostname, args.server_port)
+    chatter.set_exit_handler(chatter.stop)
     chatter.run()
 
     exit(err_ok)
