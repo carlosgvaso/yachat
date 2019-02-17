f0e8080befc14dcdf6ffff5b411d15c1b53f0779
Jose Carlos Martinez Garcia-Vaso <carlosgvaso@gmail.com>
2019-02-16 19:18:08 -0600

    Update project URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="YatChat server-client chat app.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/carlosgvaso/yachat",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
